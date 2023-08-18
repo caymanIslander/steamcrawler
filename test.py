0368404e2993f840854fe7f50f35d00d8d42e29f
caymanIslander <duman.ozgur@hotmail.com>
2023-08-19 01:02:18 +0300

    Added config file generator, will add error scraper for 404

--- test.py
+++ test.py
@@ -1,34 +1,51 @@
+import argparse
+import configparser
 import requests
-from bs4 import BeautifulSoup
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--vanityUrl', help='Steam vanity URL')
+    parser.add_argument('--id', help='SteamID')
+    return parser.parse_args()
 
+def read_config(filename):
+    config = configparser.ConfigParser()
+    config.read(filename)
+    return config
 
+def write_config(filename, config):
+    with open(filename, 'w') as config_file:
+        config.write(config_file)
 
-    
+def create_config():
+    args = parse_args()
+    config = read_config('config.ini')
 
+    selection = input("For 1: Enter Steam profile vanity url\nFor 2: Enter steam id\n")
 
-
-def fetch():
-    selection = input("For 1: Enter Steam profile custom url \n For 2: Enter steam id \n")
     if selection == "1":
-        custom_url_end = input("Enter the end of the custom URL (e.g., https://steamcommunity.com/id/\033[1mSt4ck\033[0m/): ")
-        URL = "https://steamcommunity.com/id/"+custom_url_end+"/"
-        response = requests.get(URL)
-        if response != 200:
-            soup = BeautifulSoup()
-            profile_name = soup.find('span', {'class': 'actual_persona_name'})
-            print(profile_name)
+        vanity_url_end = input("Enter the end of the vanity URL (e.g., https://steamcommunity.com/id/\033[1mSt4ck\033[0m/): ")
+        init_link = "https://steamcommunity.com/id/"+vanity_url_end+"/"
+        response = requests.get(init_link)
+        if response == 200:
+            print("Profile not found.")
+            create_config()
+        else:    
+            config['Steam']['VanityURL'] = vanity_url_end
 
     elif selection == "2":
-        URL_end = input("Enter your steam id:")
-        
-        response = requests.get(URL)
+        steam_id = input("Enter your steam id:")
+        init_link = "https://steamcommunity.com/id/"+steam_id+"/"
+        response = requests.get(init_link)
+        if response == 200 or len(steam_id) > 17:
+            print("Profile not found.")
+            create_config()
+        else:    
+            config['Steam']['ID'] = steam_id
 
-        if URL.length() != 17 or response != 200 :
-            print("Your steam id is wrong or the page is not available...")
-        else:
-            soup = BeautifulSoup()
-            profile_name = soup.find('span', {'class': 'actual_persona_name'})
-            print(profile_name)
+    write_config('config.ini', config)
+    print("Created config file.\n", config)
 
-fetch()
+
+if __name__ == '__main__':
+    create_config()
